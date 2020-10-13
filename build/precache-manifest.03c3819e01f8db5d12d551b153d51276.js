acd9dee1ab35dff40125e1dd02e278a32273f05b
Stefan Kovarbasic <kovar.zmaj@gmail.com>
2020-10-13 22:23:25 +0200

    Production build

---
+++ build/precache-manifest.03c3819e01f8db5d12d551b153d51276.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821cdc77f1180b5385d02e7f09880a16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92b951ce660af1a7f0b9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "36eaf6f0f69f37541a16",
+    "url": "/static/js/2.457a2c54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.457a2c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b951ce660af1a7f0b9",
+    "url": "/static/js/main.6bd8b7d9.chunk.js"
+  },
+  {
+    "revision": "f9bd16d13e0c62ccf1e4",
+    "url": "/static/js/runtime-main.0bde5b30.js"
+  },
+  {
+    "revision": "a336e59e44ff9e130b04cf284fd8dcad",
+    "url": "/static/media/maze.a336e59e.jpg"
+  }
+]);
